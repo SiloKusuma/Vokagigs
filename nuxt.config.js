1881772706a31ad5009e8d9a7e3440e498578518
SiloKusuma <159918505+SiloKusuma@users.noreply.github.com>
2026-06-20 14:16:43 +0700

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   mode: 'spa',
   head: {
-    title: 'Vokagigs - Platform Gig Online Terpercaya',
+    title: 'Vokagigs - Platform Gig Online',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
